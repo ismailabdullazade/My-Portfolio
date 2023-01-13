d6a3ee1576962737a8e9f262ad4f5cbb0c65f900
AAS\Ismayil.Abdullazade <ismailabdullazade715@gmail.com>
2023-01-13 23:40:53 +0400

    third commit

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,8 +1,47 @@
 import React from 'react'
+import './contact.css'
+import {MdOutlineEmail} from 'react-icons/md'
+import { BsWhatsapp } from 'react-icons/bs'
+import { RiMessengerLine } from 'react-icons/ri'
 
 const Contact = () => {
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <MdOutlineEmail className='contact__option-icon'/>
+            <h4>Email</h4>
+            <h5>ismailabdullazade715@gmail.com</h5>
+            <a href="mailto:ismailabdullazade715@gmail.com" target='_blank'>Send a message</a>
+          </article>
+
+          <article className="contact__option">
+            <RiMessengerLine className='contact__option-icon'/>
+            <h4>Messenger</h4>
+            <h5>Ismail Abdullazade</h5>
+            <a href="https://m.me/ismayil.abdullazade.18">Send a message</a>
+          </article>
+
+          <article className="contact__option">
+            <BsWhatsapp className='contact__option-icon'/>
+            <h4>WhatsApp</h4>
+            <h5>+994555550801</h5>
+            <a href="https://web.whatsapp.com/send>phone=+994515933399">Send a message</a>
+          </article>
+        </div>
+        {/* END OF CONTACT OPTIONS*/}
+        <form action="">
+          <input type="text" name='name' placeholder='Your Full Name' required />
+          <input type="email" name='name' placeholder='Your Email' required />
+          <textarea name="message" rows="7" placeholder='Your Message' required></textarea>
+          <button type='submit' className='btn btn-primary'>Send Message</button>
+        </form>
+      </div>
+    </section>
   )
 }
 
--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -5,6 +5,14 @@ import Avt2 from "../../assets/avatar2.jpg"
 import Avt3 from "../../assets/avatar3.jpg"
 import Avt4 from "../../assets/avatar4.jpg"
 
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination } from 'swiper'
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+
 const data = [
   {
     avatar: Avt1,
@@ -34,20 +42,25 @@ const Testimonials = () => {
       <h5>Review from clients</h5>
       <h2>Testimonials</h2>
 
-      <div className="container testimonials__container">
+      <Swiper className="container testimonials__container"
+           modules={[Pagination]}
+           spaceBetween={40}
+           slidesPerView={1}
+           pagination={{ clickable: true }}
+      >
         {
           data.map(({avatar,name,review},index) => {
             return(
-            <article key={index} className="testimonials">
+            <SwiperSlide key={index} className="testimonials">
               <div className="client__avatar">
                 <img src={avatar}/>
               </div>
               <h5 className="client__name">{name}</h5>
               <small className="client__review">{review}</small>
-            </article>
+            </SwiperSlide>
           )})
         }
-      </div>
+      </Swiper>
     </section>
   )
 }
