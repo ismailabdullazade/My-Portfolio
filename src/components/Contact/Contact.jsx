fdc5bd4ce2a68866fb39f12704bb21becb053cd3
AAS\Ismayil.Abdullazade <ismailabdullazade715@gmail.com>
2023-01-16 17:18:16 +0400

    footer completed

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, { useRef }from 'react'
 import './contact.css'
 import {MdOutlineEmail} from 'react-icons/md'
 import { BsWhatsapp } from 'react-icons/bs'
 import { RiMessengerLine } from 'react-icons/ri'
+import emailjs from 'emailjs-com'
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_ujqpxod', 'template_4idl4o2', form.current, '1XSSgjTX-3iwwrRgw')
+      .then((result) => {
+        console.log(result.text);
+      }, (error) => {
+        console.log(error.text);
+      });
+
+    e.target.reset();
+  }
+
   return (
     <section id='contact'>
       <h5>Get In Touch</h5>
@@ -15,7 +31,7 @@ const Contact = () => {
           <article className="contact__option">
             <MdOutlineEmail className='contact__option-icon'/>
             <h4>Email</h4>
-            <h5>ismailabdullazade715@gmail.com</h5>
+            <h6>ismailabdullazade715@gmail.com</h6>
             <a href="mailto:ismailabdullazade715@gmail.com" target='_blank'>Send a message</a>
           </article>
 
@@ -30,11 +46,11 @@ const Contact = () => {
             <BsWhatsapp className='contact__option-icon'/>
             <h4>WhatsApp</h4>
             <h5>+994555550801</h5>
-            <a href="https://web.whatsapp.com/send>phone=+994515933399">Send a message</a>
+            <a href="https://web.whatsapp.com/send>phone=+994555550801">Send a message</a>
           </article>
         </div>
         {/* END OF CONTACT OPTIONS*/}
-        <form action="">
+        <form ref={form} onSubmit={sendEmail}>
           <input type="text" name='name' placeholder='Your Full Name' required />
           <input type="email" name='name' placeholder='Your Email' required />
           <textarea name="message" rows="7" placeholder='Your Message' required></textarea>
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
+import './footer.css'
+import { FaFacebook } from 'react-icons/fa'
+import { FiInstagram } from 'react-icons/fi'
+import { IoLogoTwitter } from 'react-icons/io'
 
 const Footer = () => {
   return (
-    <section id='footer'>Footer</section>
+    <footer>
+
+      <ul className="permalinks">
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#testimonials">Testimonials</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href="https://facebook.com"><FaFacebook/></a>
+        <a href="https://instagram.com"><FiInstagram/></a>
+        <a href="https://twitter.com"><IoLogoTwitter/></a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; Ismail Abdullazade. All rights reserved.</small>
+      </div>
+
+
+    </footer>
   )
 }
 
