c9e2f1271412f8e2bb8026f744a953677631b442
IsmailGN <84373556+IsmailGN@users.noreply.github.com>
2023-07-30 14:49:56 +0400

    added new things

--- src/components/Header/CTA.jsx
+++ src/components/Header/CTA.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import CV from "../../assets/A_Ismail_CV.pdf";
+import CV from "../../assets/Ismail-CV-White.pdf";
 
 const CTA = () => {
   return (
--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import './testimonials.css'
 import ref_letter from "../../assets/Reference letter İsmayil.pdf"
-import Avt1 from "../../assets/avatar1.jpg"
-import Avt2 from "../../assets/avatar2.jpg"
-import Avt3 from "../../assets/avatar3.jpg"
-import Avt4 from "../../assets/avatar4.jpg"
+import Avt1 from "../../assets/avatar11.jpeg"
+import Avt2 from "../../assets/avatar12.png"
+// import Avt3 from "../../assets/avatar3.jpg"
+// import Avt4 from "../../assets/avatar4.jpg"
 
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Pagination } from 'swiper'
@@ -16,31 +16,32 @@ import 'swiper/css/scrollbar';
 
 const data = [
   {
+    // avatar: Avt3,
+    name: 'Eyyub Zeynalov',
+    address:'eyyub.Z@aplusa-security.com',
+    position:"Manager of Support and Development department at 'A+A Security LLC'",
+    review: 'Ismail is dedicated to providing quality work. He checks and rechecks his software before turning it in for presentation to the client. He strives to make his programs as user friendly as possible understanding that not everyone is an expert programmer. He is detail oriented and rarely has any bugs in his finished product. Dean’s energy is limitless and his enthusiasm serves to motivate everyone on his team. He will be greatly missed at "A+A Security LLC" but we know he will succeed wherever he goes.'
+  },
+  {
     avatar: Avt1,
-    name: 'Tina Snow',
-    review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum id veniam, voluptate dicta autem quibusdam inventore hic nihil fugiat laboriosam est illo eius modi praesentium illum facere repellat odio! Quod?'
+    name: 'Kamran Jabiev',
+    address:'kamran.jabiyev@code.edu.az',
+    position:".Net Developer and Senior .Net Instructure at 'Code Academy'",
+    review: "Ismail takes his work very seriously and regularly enrolled in classes to stay abreast of the newest developments in programming.His ongoing professional development courses helped him to easily learn new techniques and perform complex tasks the first time around. Although Ismail is a fast-learner, he is very careful and cautious about his work.I strongly recommend Ismail for any mid-level development position. And with the right guidance and training, I have no doubt Ismail could excel in an upper-level development position. He's simply that good."
   },
   {
     avatar: Avt2,
-    name: 'Shatte Male',
-    review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum id veniam, voluptate dicta autem quibusdam inventore hic nihil fugiat laboriosam est illo eius modi praesentium illum facere repellat odio! Quod?'
+    name: 'Nijat Talibov',
+    position: 'Senior Fullstack developer(7+ years experience)',
+    review: 'Ismail is passionate about writing code and solving clients’ problems through the development of creative and innovative software. He has tremendous potential that is not being fulfilled in his current position.He is friendly, easy to get along with, well-liked by our clients and respected by his co-workers.'
   },
-  {
-    avatar: Avt3,
-    name: 'Kwame Suoi',
-    review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum id veniam, voluptate dicta autem quibusdam inventore hic nihil fugiat laboriosam est illo eius modi praesentium illum facere repellat odio! Quod?'
-  },
-  {
-    avatar: Avt4,
-    name: 'Totiw Potiw',
-    review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum id veniam, voluptate dicta autem quibusdam inventore hic nihil fugiat laboriosam est illo eius modi praesentium illum facere repellat odio! Quod?'
-  },
+
 ]
 
 const Testimonials = () => {
   return (
     <section id='testimonials'>
-      <h5>Review from clients</h5>
+      {/* <h5>Review from clients</h5> */}
       {/* <h2>Testimonials</h2> */}
 
       <div className='cta cta_ref_let'>
@@ -55,14 +56,19 @@ const Testimonials = () => {
            pagination={{ clickable: true }}
       >
         {
-          data.map(({avatar,name,review},index) => {
+          data.map(({avatar,name,position,review,address},index) => {
             return(
             <SwiperSlide key={index} className="testimonials">
               <div className="client__avatar">
                 <img src={avatar}/>
               </div>
               <h5 className="client__name">{name}</h5>
+              <h4 className="client__name">{position}</h4>
+
               <small className="client__review">{review}</small>
+              <a className="client__name">{address}</a>
+              
+
             </SwiperSlide>
           )})
         }
