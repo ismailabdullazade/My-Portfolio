d6c138e5854a0d9ee1881ce5676d0cf93a5f57e0
IsmailGN <84373556+IsmailGN@users.noreply.github.com>
2024-02-02 17:17:41 +0400

    added version

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -19,7 +19,7 @@ const Portfolio = () => {
           <div className="portfolio__item-image">
             <img src={IMG7} alt="" />
           </div>
-          <h3>Next-Auth</h3>
+          <h3>Next-Auth v5</h3>
           <div className="portfolio__item-cta">
             <a href="https://github.com/ismailabdullazade/Next-Auth-MasterClass" className='btn' target="_blank">Github</a>
             <a href="https://next-auth-master-class-hw2y.vercel.app" className='btn btn-primary' target="_blank">Live</a>
